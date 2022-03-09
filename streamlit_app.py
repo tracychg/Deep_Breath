65caa382e820e2f7f832e2a57c869b0ac6dfe0aa
Cedric De Schaut <cedricdeschaut1@gmail.com>
2022-03-09 16:53:46 +0000

    info on disease

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,12 +89,48 @@ if app_mode == disease_classification_page:
             y, sr = handle_uploaded_audio_file(file_uploader)
             spectrogram = from_audiofile_to_spectrogram(y)
             pred = model.predict(spectrogram)
-            result_dict = {pred[0][0]:"COPD",
+            result_dict = {pred[0][0]:'''**COPD** \n
+
+Chronic obstructive pulmonary disease (COPD) is the name for a group of lung conditions that cause breathing difficulties.
+
+                            It includes:
+
+                            emphysema – damage to the air sacs in the lungs
+                            chronic bronchitis – long-term inflammation of the airways
+                            COPD is a common condition that mainly affects middle-aged or older adults who smoke. Many people do not realise they have it.
+
+                            The breathing problems tend to get gradually worse over time and can limit your normal activities, although treatment can help keep the condition under control.'''
+                           ,
                 pred[0][1]:"Healthy",
-                pred[0][2]:"URTI",
-                pred[0][3]:"Bronchiectasis",
-                pred[0][4]:"Bronchiolitis",
-                pred[0][5]:"Pneumonia"}
+                pred[0][2]:'''**URTI** \n
+
+A URTI is a viral infection that can affect the nose, throat and sinuses.
+                            Upper respiratory tract infections are extremely common and there are many different viruses which cause them.
+                            Children are particularly susceptible to these and it is normal for
+                            children under five years old to have as many as twelve URTIs in one year. The frequency of these illnesses
+                            reduces as children get older – for example a child in primary school may get around six URTIs per year.''',
+                pred[0][3]:'''**Bronchiectasis**
+
+Bronchiectasis is a long-term condition where the airways of the lungs become widened, leading to a build-up of excess mucus that can make the lungs more vulnerable to infection.
+
+                            The most common symptoms of bronchiectasis include:
+
+                            a persistent cough that usually brings up phlegm (sputum)
+                            shortness of breath
+                            The severity of symptoms can vary widely. Some people have only a few symptoms that do not appear often, while others have wide-ranging daily symptoms.
+
+                            The symptoms tend to get worse if you develop an infection in your lungs.''',
+                pred[0][4]:'''**Bronchiolitis**
+
+Bronchiolitis is a common lower respiratory tract infection that affects babies and young children under 2 years old.
+
+                            Most cases are mild and clear up within 2 to 3 weeks without the need for treatment, although some children have severe symptoms and need hospital treatment.
+
+                            The early symptoms of bronchiolitis are similar to those of a common cold, such as a runny nose and a cough.''',
+                pred[0][5]:'''**Pneumonia**
+
+Pneumonia is swelling (inflammation) of the tissue in one or both lungs. It's usually caused by a bacterial infection.
+                            It can also be caused by a virus, such as coronavirus (COVID-19).'''}
 
             max = max(pred[0])
             max2 = 0
@@ -103,20 +139,9 @@ if app_mode == disease_classification_page:
                         max2 = v
 
             if max - max2 >0.2:
-                st.text("The pattern that the model detected looks most similar to the pattern of " + result_dict.get(max))
+                st.markdown(f"The pattern that the model detected looks most similar to the pattern of {result_dict.get(max)}")
+                st.markdown(
+                f"""<a href="https://www.england.nhs.uk/ourwork/clinical-policy/respiratory-disease//">Read more</a>""", unsafe_allow_html=True,
+)
             else:
-                st.text("The model cannot map the detected patterns to a disease with high confidence. It is doubting between " + result_dict.get(max) + " and " + result_dict.get(max2))
-
-
-
-
-
-
-
-# today_date = st.date_input('What is the date of the day?', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-# name = st.text_input("What is the name of the patient")
-# gender = st.radio("What is the gender of the patient?",["male","female","else"])
-# age = st.number_input("What is the age of the patient?")
-# height = st.number_input("What's the height of the patient in m?")
-# weight = st.number_input("What's the weight of the patient in kg")
-# sound = st.file_uploader("Please upload the breathing sound of the patient:")
+                st.markdown("The model cannot map the detected patterns to a disease with high confidence. It is doubting between " + result_dict.get(max) + " and " + result_dict.get(max2))
